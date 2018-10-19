f3568398a8c269f78cbacc5a18723c761e4a747e
wangjitao <767884538@qq.com>
2018-10-19 16:11:41 +0800

    忙忙碌碌

--- app/src/main/java/com/wang/gradletest/MainActivity.java
+++ app/src/main/java/com/wang/gradletest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.wang.gradletest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.wang.youzhi.VersionUtil;
 
 /**
  * @author wangjt
@@ -12,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.bt1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, VersionUtil.getLibVersion(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
