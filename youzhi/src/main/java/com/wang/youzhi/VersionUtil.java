6e770b947474270af90ffa23fad1c54a47f19349
wangjitao <767884538@qq.com>
2018-10-19 16:46:33 +0800

    版本1.2

--- youzhi/src/main/java/com/wang/youzhi/VersionUtil.java
+++ youzhi/src/main/java/com/wang/youzhi/VersionUtil.java
@@ -8,8 +8,9 @@ public class VersionUtil {
 
     private static String VERSION = "我是版本_v1.0";
     private static String VERSION1 = "我是版本_v1.1";
+    private static String VERSION2 = "我是版本_v1.2";
 
     public static String getLibVersion() {
-        return VERSION1;
+        return VERSION2;
     }
 }
