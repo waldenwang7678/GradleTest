05fd1a2eaafa73240d1719b3c3fbcbd719f359d8
wangjitao <767884538@qq.com>
2018-10-19 15:03:23 +0800

    first commit

---
+++ app/src/main/java/com/wang/gradletest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wang.gradletest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author wangjt
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ youzhi/src/main/java/com/wang/youzhi/VersionUtil.java
@@ -0,0 +1,15 @@
+package com.wang.youzhi;
+
+/**
+ * @author wangjt
+ * @date 2018/10/19
+ */
+public class VersionUtil {
+
+    private static String VERSION = "1.0";
+    private static String VERSION1 = "1.1";
+
+    public static String getLibVersion() {
+        return VERSION;
+    }
+}
